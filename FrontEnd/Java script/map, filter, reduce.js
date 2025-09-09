36a29358339e5d61cb3fb12c74703585ece06702
pedrochoks <pedro.a.gomes12@aluno.senai.br>
2025-09-09 10:29:32 -0300

    09/09

--- FrontEnd/Java script/map, filter, reduce.js
+++ FrontEnd/Java script/map, filter, reduce.js
@@ -64,7 +64,7 @@
 // const numerosDobrados = numero.map(numero => numero* 2)
 // console.log(numerosDobrados);
 
-//Filtrando pessoas
+//* Filtrando pessoas
 
 // let pessoas = [
 //     {nome: "Lucas", idade: 17},
@@ -99,15 +99,55 @@
 // console.log(soma);
 
 //* Verificar o maior numero de um array
-const numeros = [10, 5, 8, 20, 3]
+// const numeros = [10, 5, 8, 20, 3]
 
-const maiorNumero = numeros.reduce((max, numero) => {
-    if (numero >= max) {
-        return numero;
-    }
-    else {
-        return max;
-    }
-}, numeros[0]
-)
-console.log(maiorNumero)
+// const maiorNumero = numeros.reduce((max, numero) => {
+//     if (numero >= max) {
+//         return numero;
+//     }
+//     else {
+//         return max;
+//     }
+// }, numeros[0]
+// )
+// console.log(maiorNumero)
+
+//* Contar a frequencia de palavras
+// const palavras = ["maca", "banana", "maca", "laranja", "banana", "maca"]
+
+// const contagem = palavras.reduce((acumulador, palavra) => {acumulador[palavra] = (acumulador[palavra] || 0 ) + 1; return acumulador}, {})
+
+// console.log(contagem);
+
+// //* Calcular media de notas
+
+// const notas = [7, 8, 9, 6, 10]
+
+// const media = notas.reduce((total, nota) => total + nota, 0 ) / notas.length;
+
+// console.log(media);
+
+
+//* USO COMBINADO
+const usuarios = [
+    {id: 1, nome: "Alice", idade: 25},
+    {id: 2, nome: "Bob", idade: 30},
+    {id: 3, nome: "Carol", idade: 20}
+]
+
+//? FIltrar usuarios com idade maior que 21
+const maioresDeIdade = usuarios.filter(usuario => usuario.idade > 21);
+
+//? Encontrar o primeiro usuario com idade maior que
+const usuarioIdade = usuarios.find(usuario => usuario.idade > 21);
+
+//? Criar um array apenas com nomes de usuarios
+const nomesUsuarios = usuarios.map(usuario => usuario.nome)
+
+//? Somar todas as idades do usuarios usando o reduce
+const somaIdades = usuarios.reduce((total, usuario) => total + usuario.idade , 0)
+
+console.log("Maiores de idade: ", maioresDeIdade)
+console.log("Primeiro maior de idade :", usuarioIdade)
+console.log("Nomes de usuarios: ", nomesUsuarios)
+console.log("Soma de idades: ", somaIdades)
