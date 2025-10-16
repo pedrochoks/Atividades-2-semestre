13e57f504f91923c7e39f9ce49fbec84d553c5f6
pedrochoks <pedro.a.gomes12@aluno.senai.br>
2025-10-16 10:17:31 -0300

    Procedure

--- Banco de Dados/Procedure.sql
+++ Banco de Dados/Procedure.sql
@@ -1,3 +1,4 @@
+use VendasOnline
 -- Procedures
 
 --Procedure 1: Inserir CLientes
@@ -55,3 +56,86 @@ end
 select * from Produto;
 EXEC AtualizarPrecoProduto 2, 10
 Select * from Produto;
+
+-- Procedure 4: Total de vendas por cliente (saida)
+create procedure ObterTotalVendas
+	@ClienteId INT,
+	@TotalVendas Decimal(10,2) OUTPUT
+AS
+BEGIN
+	Set NOCOUNT ON
+	SELECT @TotalVendas = coalesce(sum(ValorTotal),0)
+	-- Substitui valores nulos por zero
+	from Pedido
+	Where ClienteId = @ClienteId
+END
+
+-- Testando Procedure:
+declare @Total Decimal(10,2)
+EXEC ObterTotalVendas 1, @Total OUTPUT;
+SELECT @Total As TotalDeVendas
+
+select * from Pedido
+Where ClienteId = 2
+
+-- Procedure 5: Relatorio de vendas por cliente
+create procedure VendasPorCliente
+	@DataLimite Date
+as
+begin
+	Set nocount on
+
+	select C.ClienteId, C.Nome,
+	SUM(D.Quantidade * D.PrecoUnitario) AS TotalGasto
+	From Cliente C
+	join Pedido P			ON P.ClienteId = C.ClienteId
+	join DetalhesPedido D	ON D.PedidoId = P.PedidoId
+	Where P.DataPedido < @DataLimite
+	group by C.ClienteId, C.Nome
+end
+
+-- Testando Procedure
+Insert into DetalhesPedido (PedidoId, ProdutoId, Quantidade, PrecoUnitario)
+values
+(1, 1, 1, 4500.00)
+
+SELECT * FROM Pedido
+
+Exec VendasPorCliente '2025-10-14'
+
+-- Procedure 6: Historico de preco com tabela propria
+
+create table HistoricoPrecos (
+	HistoricoId INT IDENTITY PRIMARY KEY,
+	ProdutoId INT FOREIGN KEY REFERENCES Produto(ProdutoId),
+	PrecoAntigo Decimal(10,2),
+	PrecoNovo Decimal(10,2),
+	DataModificacao date
+);
+
+Create Procedure AtualizarPrecoProdutoComHistorico
+	@ProdutoId int,
+	@NovoPreco decimal(10,2)
+as
+begin
+	set nocount on
+	Declare @PrecoAntigo decimal(10,2)
+
+	SELECT @PrecoAntigo = preco
+	FROM Produto
+	Where ProdutoId = @ProdutoId;
+
+	UPDATE Produto set Preco = @NovoPreco
+	Where ProdutoId = @ProdutoId
+	
+	INSERT INTO HistoricoPrecos 
+	(ProdutoId,PrecoAntigo, PrecoNovo, DataModificacao)
+	values (@ProdutoId, @PrecoAntigo, @NovoPreco, 
+	DateADD(hour, -3, SYSUTCDATETIME()))
+end
+
+--Testando Procedure:
+exec AtualizarPrecoProdutoComHistorico 2, 75.00
+
+select * From Produto
+select * FROM HistoricoPrecos;
